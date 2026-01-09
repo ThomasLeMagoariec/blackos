c25a8b82560816f50ab50acc4f09d6b680d619b3
Thomas <thomas.lemagoariec@outlook.com>
2026-01-09 19:56:56 +0100

    added kb state

--- src/keyboard.h
+++ src/keyboard.h
@@ -6,12 +6,21 @@
 
 #define MAX_KB_SIZE 256
 
-void kb_enqueue_key(uint8_t scancode);
-int kb_dequeue_key();
+// kb map
 char kb_map_scancode(uint8_t scancode);
 void kb_handle_scancode(uint8_t scancode);
+
+// kb buffer
+void kb_enqueue_key(uint8_t scancode);
+int kb_dequeue_key();
 char* kb_get_buffer();
 void kb_clear_buffer();
 uint8_t kb_get_size();
+
+// kb state
+void kb_disable();
+void kb_enable();
+void kb_toggle();
+uint8_t kb_state();
 
 #endif
